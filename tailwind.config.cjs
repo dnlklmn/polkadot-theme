5d34ea7889e46fe5e5f47781986bbcc0da162283
dnlklmn <pelicansinberlin@gmail.com>
2023-05-11 13:35:51 +0200

    package update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,14 +8,14 @@ module.exports = {
       boxShadow: {
         pop: "var(--box-shadow-pop)",
         idle: "var(--box-shadow-idle)",
-        hover: "var(--box-shadow-hover)"
+        hover: "var(--box-shadow-hover)",
       },
       colors: {
         background: {
           default: "var(--colors-background-default)",
           system: "var(--colors-background-system)",
           float: "var(--colors-background-float)",
-          dip: "var(--colors-background-dip)"
+          dip: "var(--colors-background-dip)",
         },
         fill: {
           primary: "var(--colors-fill-primary)",
@@ -32,11 +32,11 @@ module.exports = {
           danger: "var(--colors-fill-danger)",
           dangerHover: "var(--colors-fill-danger-hover)",
           dangerPressed: "var(--colors-fill-danger-pressed)",
-      "elevate_1": "var(--colors-fill-elevate-1)",
+          elevate_1: "var(--colors-fill-elevate-1)",
           overlay: "var(--colors-fill-overlay)",
           selected: "var(--colors-fill-selected)",
           white: "var(--colors-fill-white)",
-          separator: "var(--colors-fill-separator)"
+          separator: "var(--colors-fill-separator)",
         },
         border: {
           contrast: "var(--colors-border-contrast)",
@@ -45,7 +45,7 @@ module.exports = {
           danger: "var(--colors-border-danger)",
           hint: "var(--colors-border-hint)",
           disabled: "var(--colors-border-disabled)",
-          focus: "var(--colors-border-focus)"
+          focus: "var(--colors-border-focus)",
         },
         foreground: {
           contrast: "var(--colors-foreground-contrast)",
@@ -55,13 +55,13 @@ module.exports = {
           white: "var(--colors-foreground-white)",
           disabled: "var(--colors-foreground-disabled)",
           danger: "var(--colors-foreground-danger)",
-          emphasized: "var(--colors-foreground-emphasized)"
+          emphasized: "var(--colors-foreground-emphasized)",
         },
         accent: {
           cyan: "var(--colors-accent-cyan)",
           green: "var(--colors-accent-green)",
           lime: "var(--colors-accent-lime)",
-          random: "var(--colors-accent-random)"
+          random: "var(--colors-accent-random)",
         },
         black: "var(--colors-black)",
         white: "var(--colors-white)",
@@ -76,7 +76,7 @@ module.exports = {
           700: "var(--colors-p-pink-700)",
           800: "var(--colors-p-pink-800)",
           900: "var(--colors-p-pink-900)",
-          950: "var(--colors-p-pink-950)"
+          950: "var(--colors-p-pink-950)",
         },
         pGray: {
           50: "var(--colors-p-gray-50)",
@@ -89,7 +89,7 @@ module.exports = {
           700: "var(--colors-p-gray-700)",
           800: "var(--colors-p-gray-800)",
           900: "var(--colors-p-gray-900)",
-          950: "var(--colors-p-gray-950)"
+          950: "var(--colors-p-gray-950)",
         },
         pRed: {
           50: "var(--colors-p-red-50)",
@@ -102,7 +102,7 @@ module.exports = {
           700: "var(--colors-p-red-700)",
           800: "var(--colors-p-red-800)",
           900: "var(--colors-p-red-900)",
-          950: "var(--colors-p-red-950)"
+          950: "var(--colors-p-red-950)",
         },
         pWhiteTransparent: {
           100: "var(--colors-p-white-transparent-100)",
@@ -113,7 +113,7 @@ module.exports = {
           600: "var(--colors-p-white-transparent-600)",
           700: "var(--colors-p-white-transparent-700)",
           800: "var(--colors-p-white-transparent-800)",
-          900: "var(--colors-p-white-transparent-900)"
+          900: "var(--colors-p-white-transparent-900)",
         },
         pBlackTransparent: {
           50: "var(--colors-p-black-transparent-50)",
@@ -125,22 +125,22 @@ module.exports = {
           600: "var(--colors-p-black-transparent-600)",
           700: "var(--colors-p-black-transparent-700)",
           800: "var(--colors-p-black-transparent-800)",
-          900: "var(--colors-p-black-transparent-900)"
+          900: "var(--colors-p-black-transparent-900)",
         },
         accentCyan: {
           500: "var(--colors-accent-cyan-500)",
           600: "var(--colors-accent-cyan-600)",
-          700: "var(--colors-accent-cyan-700)"
+          700: "var(--colors-accent-cyan-700)",
         },
         accentGreen: {
           500: "var(--colors-accent-green-500)",
           600: "var(--colors-accent-green-600)",
-          700: "var(--colors-accent-green-700)"
+          700: "var(--colors-accent-green-700)",
         },
         accentLime: {
           500: "var(--colors-accent-lime-500)",
           600: "var(--colors-accent-lime-600)",
-          700: "var(--colors-accent-lime-700)"
+          700: "var(--colors-accent-lime-700)",
         },
         pPurple: {
           50: "var(--colors-p-purple-50)",
@@ -154,26 +154,26 @@ module.exports = {
           800: "var(--colors-p-purple-800)",
           900: "var(--colors-p-purple-900)",
           925: "var(--colors-p-purple-925)",
-          950: "var(--colors-p-purple-950)"
-        }
+          950: "var(--colors-p-purple-950)",
+        },
       },
       fontFamily: {
         unbounded: "var(--font-family-unbounded)",
         inter: "var(--font-family-inter)",
-        mono: "var(--font-family-mono)"
+        mono: "var(--font-family-mono)",
       },
       lineHeight: {
-    "h2": "var(--line-height-h2)",
-    "h1": "var(--line-height-h1)",
-    "h3": "var(--line-height-h3)",
-    "h4": "var(--line-height-h4)",
-    "h5": "var(--line-height-h5)",
-    "h6": "var(--line-height-h6)",
+        h2: "var(--line-height-h2)",
+        h1: "var(--line-height-h1)",
+        h3: "var(--line-height-h3)",
+        h4: "var(--line-height-h4)",
+        h5: "var(--line-height-h5)",
+        h6: "var(--line-height-h6)",
         body: "var(--line-height-body)",
-    "body2": "var(--line-height-body2)",
+        body2: "var(--line-height-body2)",
         subtitle: "var(--line-height-subtitle)",
-    "subtitle2": "var(--line-height-subtitle2)",
-        caption: "var(--line-height-caption)"
+        subtitle2: "var(--line-height-subtitle2)",
+        caption: "var(--line-height-caption)",
       },
       fontWeight: {
         unboundedBold: "var(--font-weight-unbounded-bold)",
@@ -182,35 +182,35 @@ module.exports = {
         interMedium: "var(--font-weight-inter-medium)",
         interBold: "var(--font-weight-inter-bold)",
         monoRegular: "var(--font-weight-mono-regular)",
-        monoMedium: "var(--font-weight-mono-medium)"
+        monoMedium: "var(--font-weight-mono-medium)",
       },
       fontSize: {
-    "body2": "var(--font-size-body2)",
+        body2: "var(--font-size-body2)",
         body: "var(--font-size-body)",
-    "h6": "var(--font-size-h6)",
-    "h5": "var(--font-size-h5)",
-    "h4": "var(--font-size-h4)",
-    "h3": "var(--font-size-h3)",
-    "h2": "var(--font-size-h2)",
-    "h1": "var(--font-size-h1)",
+        h6: "var(--font-size-h6)",
+        h5: "var(--font-size-h5)",
+        h4: "var(--font-size-h4)",
+        h3: "var(--font-size-h3)",
+        h2: "var(--font-size-h2)",
+        h1: "var(--font-size-h1)",
         subtitle: "var(--font-size-subtitle)",
-    "subtitle2": "var(--font-size-subtitle2)",
-        caption: "var(--font-size-caption)"
+        subtitle2: "var(--font-size-subtitle2)",
+        caption: "var(--font-size-caption)",
       },
       letterSpacing: {
         0: "var(--letter-spacing-0)",
-        1: "var(--letter-spacing-1)"
+        1: "var(--letter-spacing-1)",
       },
       paragraphSpacing: {
-        0: "var(--paragraph-spacing-0)"
+        0: "var(--paragraph-spacing-0)",
       },
       textCase: {
-        none: "var(--text-case-none)"
+        none: "var(--text-case-none)",
       },
       textDecoration: {
-        none: "var(--text-decoration-none)"
-      }
+        none: "var(--text-decoration-none)",
+      },
     },
   },
-  plugins: [require("@tailwindcss/forms")]
-}
+  plugins: [require("@tailwindcss/forms")],
+};
